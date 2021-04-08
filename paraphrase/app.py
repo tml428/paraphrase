3427e36cfc15aef994994607521abe524897dfe9
Michelle Lee <tl428@cornell.edu>
2021-04-08 15:55:20 -0400

    fixup! refactor: add other params

--- paraphrase/app.py
+++ paraphrase/app.py
@@ -5,123 +5,139 @@ import random
 import math
 import json
 import base64
+import re
 
 tokenizer = AutoTokenizer.from_pretrained("model/")
 model = AutoModelForSeq2SeqLM.from_pretrained("model/")
 
+
 def get_decoded_output(output):
     return tokenizer.decode(output, skip_special_tokens=True, clean_up_tokenization_spaces=True)
 
+
 def get_normalized_rouge_output(output, maxProb, minProb):
-    print(output)
-    if  output[1] > minProb and output[1] < maxProb:
+    if minProb < output[1] and output[1] < maxProb:
         return True
     return False
 
-scorer = rouge_scorer.RougeScorer(['rougeL'], use_stemmer=True)
 
-def predict(original_sentences, method="beam", maxProb = 0.9, minProb = 0.7):
+scorer = rouge_scorer.RougeScorer(["rougeL"], use_stemmer=True)
+
+
+def predict(
+    original_sentences, method="beam", maxProb=0.9, minProb=0.7, variables=[], topP=0.9, topK=120, kpTemperature=0.4
+):
     paraphrased_content = []
-    
+    inputOutputCandidates = []
+
     for sentence in original_sentences:
         # comment to suppress output
         # print('original_sentence: ' + sentence)
-        
-        text =  "paraphrase: " + sentence + " </s>"
+
+        text = "paraphrase: " + sentence + " </s>"
         encoding = tokenizer.encode_plus(text, pad_to_max_length=True, return_tensors="pt")
         input_ids, attention_masks = encoding["input_ids"], encoding["attention_mask"]
 
-        
         if method == "top_k":
             outputs = model.generate(
-                input_ids=input_ids, attention_mask=attention_masks,
+                input_ids=input_ids,
+                attention_mask=attention_masks,
                 max_length=256,
                 early_stopping=True,
-
                 # this block include args to using top k/p samplings
                 do_sample=True,
-                top_k=120,
-                top_p=maxProb,
-                temperature=minProb
+                top_k=topK,
+                top_p=topP,
+                temperature=kpTemperature,
             )
         else:
             outputs = model.generate(
-                input_ids=input_ids, attention_mask=attention_masks,
+                input_ids=input_ids,
+                attention_mask=attention_masks,
                 max_length=256,
                 early_stopping=True,
-
                 # this block include args to using beam search
                 num_return_sequences=10,
                 num_beams=50,
                 num_beam_groups=50,
-                diversity_penalty=0.4
+                diversity_penalty=0.4,
             )
 
         decoded_ouputs = list(map(get_decoded_output, outputs))
-        print(decoded_ouputs)
+
         rouge_scores = []
         for predicted_output in decoded_ouputs:
             score = scorer.score(sentence, predicted_output)
-            score = score['rougeL'][2]
+            score = score["rougeL"][2]
             rouge_scores.append(score)
-
         combined_results = zip(decoded_ouputs, rouge_scores)
-        candidates = list(filter(lambda result: get_normalized_rouge_output(result, maxProb, minProb), combined_results))
+        candidates = list(
+            filter(lambda result: get_normalized_rouge_output(result, maxProb, minProb), combined_results)
+        )
+        inputOutputCandidates.append({"original_sentence": sentence, "candidates": candidates})
+
         # print(candidates)
 
         if candidates:
-            paraphrased_content.append(candidates[math.floor(random.random()*len(candidates))][0])
+            paraphrasedSentence = candidates[math.floor(random.random() * len(candidates))][0]
             # comment to suppress output
             # print('winner: ' + winner[0])
-        else: 
-            paraphrased_content.append(sentence)
+        else:
+            paraphrasedSentence = sentence
             # comment to suppress output
             # print('sentence: '+ sentence)
+        paraphrased_content.append(paraphrasedSentence)
 
-    return (' '.join(paraphrased_content))
+    paraphrasedOutput = " ".join(paraphrased_content)
+    for variable in variables:
+        paraphrasedOutput = re.sub(
+            "(" + variable["variableKey"] + ")",
+            variable["variableValue"] + "\\1" + "</a>",
+            paraphrasedOutput,
+            0,
+            re.IGNORECASE,
+        )
+
+    return {"paraphrased_content": paraphrasedOutput, "inputOutputCandidates": inputOutputCandidates}
+
+
+def constructResponse(statusCode, responseBody):
+    return {
+        "statusCode": statusCode,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Accept": "*/*",
+            "Accept-Encoding": "gzip, deflate",
+            "Access-Control-Allow-Headers": "*",
+        },
+        "body": json.dumps(responseBody),
+    }
+
+
+def getPredictedResponse(requestBody):
+    return predict(
+        requestBody["original_content"],
+        requestBody["method"],
+        requestBody["max_prob"],
+        requestBody["min_prob"],
+        requestBody["variables"],
+        requestBody["topP"],
+        requestBody["topK"],
+        requestBody["kpTemperature"],
+    )
 
 
 def paraphrase(event, context):
     try:
-        body = json.loads(event['body'])
-        paraphrased_content = predict(body['original_content'], body['method'], body['max_prob'], body['min_prob'])
+        body = json.loads(event["body"])
+        paraphrased_content = getPredictedResponse(body)
+        return constructResponse(200, paraphrased_content)
 
-        return {
-            "statusCode": 200,
-            "headers": {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                'Accept': '*/*',
-                'Accept-Encoding': 'gzip, deflate',
-                'Access-Control-Allow-Headers': '*'
-            },
-            "body": json.dumps({'paraphrased_content': paraphrased_content})
-        }
     except Exception as e:
         try:
-            body = json.loads(base64.b64decode(event['body']))
-            paraphrased_content = predict(body['original_content'], body['method'], body['max_prob'], body['min_prob'])
-
-            return {
-                "statusCode": 200,
-                "headers": {
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*',
-                    'Accept': '*/*',
-                    'Accept-Encoding': 'gzip, deflate',
-                    'Access-Control-Allow-Headers': '*'
-                },
-                "body": json.dumps({'paraphrased_content': paraphrased_content})
-            }
+            body = json.loads(base64.b64decode(event["body"]))
+            paraphrased_content = getPredictedResponse(body)
+            return constructResponse(200, paraphrased_content)
         except Exception as e:
-            return {
-                "statusCode": 500,
-                "headers": {
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*',
-                    'Accept': '*/*',
-                    'Accept-Encoding': 'gzip, deflate',
-                    'Access-Control-Allow-Headers': '*'
-                },
-                "body": json.dumps({"error": repr(e)})
-            }
+            return constructResponse(500, {"error": repr(e)})
